b69d4b4514c96612afadb69197f56185ebd8f4b6
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2025-05-01 22:30:52 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# arconia-cli-flake
+# Arconia CLI - Nix Flake
+
+A [Nix Flake](https://nixos.wiki/wiki/flakes) to install the [Arconia CLI](https://arconia.io/docs/arconia-cli/latest/index.html) in your Nix-based environment.
+
+## How to use it
+
+### Flox
+
+Using [Flox](https://flox.dev), you can install the Arconia CLI in your development environment as follows:
+
+```shell
+flox install github:thomasvitale/arconia-cli-flake
+```
+
+## How to build it
+
+Using [nix], you can build the flake and generate a lock file. Whenever the flake is updated with a new version of Arconia CLI, this command needs to be run again.
+
+```shell
+nix build --extra-experimental-features flakes --extra-experimental-features nix-command
+```
---
+++ flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "Arconia CLI";
+ 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+ 
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+ 
+          arconiaFor = {
+            x86_64-linux = {
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.2.4/arconia-cli-0.2.4-linux-amd64.zip";
+              sha256 = "17528d087aee012dea7adf28c3757c1ac46d15b2d3ec7faa79cb4b10e39e7c8a";
+            };
+            aarch64-linux = {
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.2.4/arconia-cli-0.2.4-linux-amd64.zip";
+              sha256 = "17528d087aee012dea7adf28c3757c1ac46d15b2d3ec7faa79cb4b10e39e7c8a";
+            };
+            x86_64-darwin = {
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.2.4/arconia-cli-0.2.4-macos-amd64.zip";
+              sha256 = "54adacc8d398c8af74b8592ca94d8eaba8ec272016851b500c601478f18cb726";
+            };
+            aarch64-darwin = {
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.2.4/arconia-cli-0.2.4-macos-aarch64.zip";
+              sha256 = "6a049d0d577e65d942aec5c070cae059c994d2ba4d7547ba7281bf169bc11c10";
+            };
+          };
+          arconia = pkgs.stdenv.mkDerivation {
+            pname = "arconia";
+            version = "0.2.4";
+            src = pkgs.fetchurl arconiaFor.${system};
+            nativeBuildInputs = [ pkgs.unzip ];
+            dontStrip = true;
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out/
+              ${if pkgs.stdenv.isLinux then ''
+              patchelf \
+                --set-rpath "${pkgs.stdenv.cc.cc.lib}/lib" \
+                --set-interpreter "$(cat $NIX_CC/nix-support/dynamic-linker)" \
+                $out/bin/node
+              '' else ""}
+            '';
+          };
+        in
+        {
+          default = arconia;
+        }
+      );
+    };
+}

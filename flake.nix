712070d8c4f5b8679dc963e1fb208c4d53c27a58
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2026-04-02 17:05:57 +0200

    deps: Update Arconia CLI 0.12.2 to 0.12.3
    Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

--- flake.nix
+++ flake.nix
@@ -17,21 +17,21 @@
  
           arconiaFor = {
             x86_64-linux = {
-              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.2/arconia-cli-0.12.2-linux-amd64.zip";
-              hash = "sha256-EuC8UjGBkCtM82Pa+MQkNxuaQxuOjg0+8/QJ9Ok3D68=";
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.3/arconia-cli-0.12.3-linux-amd64.zip";
+              hash = "sha256-MjUss35IkW77kA13Z/u101skfmklnqLBwKjS67hQN/Y=";
             };
             aarch64-linux = {
-              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.2/arconia-cli-0.12.2-linux-aarch64.zip";
-              hash = "sha256-Lgnt9TEd57k/GA9rVmNw4hlN5xaXJpyYYO3r2ZX8l0Q=";
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.3/arconia-cli-0.12.3-linux-aarch64.zip";
+              hash = "sha256-MHM2ERHbKAXvZnd1zVhZvk9MWyfiRnPplGPq2LMGW6I=";
             };
             aarch64-darwin = {
-              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.2/arconia-cli-0.12.2-macos-aarch64.zip";
-              hash = "sha256-9Ju99cuZwoGOVRbJ0Asi+2L7Z25Xskahmc1tFrAE6iQ=";
+              url = "https://github.com/arconia-io/arconia-cli/releases/download/v0.12.3/arconia-cli-0.12.3-macos-aarch64.zip";
+              hash = "sha256-ugsn0XaFD2HnraCfGzaJxQDL2gKveSKuxB3rXutvfkk=";
             };
           };
           arconia = pkgs.stdenv.mkDerivation {
             pname = "arconia";
-            version = "0.12.2";
+            version = "0.12.3";
             src = pkgs.fetchurl arconiaFor.${system};
             nativeBuildInputs = [ pkgs.unzip ];
             dontStrip = true;
